a438351906f7204559f44d3e268cbff98812ee43
PaulMartin98 <44432564+PaulMartin98@users.noreply.github.com>
2021-03-23 21:26:55 +0100

    Add files via upload

--- scripts/spectrum.py
+++ scripts/spectrum.py
@@ -1,55 +1,79 @@
 import numpy as np
 from collections import Counter
 
-"""
-Spectrum Kernel
-"""
-
-
 class SpectrumKernel:
-    def __init__(self, X_train, seq_length):
+    def __init__(self, seq_length, normalization=True):
         """
-        :param X_train: array
-        :param seq_length: int, length of subsequences to consider inside ARN
+        :param X_train: array\n",
+        :param seq_length: int, length of subsequences to consider inside ARN\n",
         """
-        self.X_train = X_train
         self.seq_length = seq_length
+        self.normalization = normalization
 
     def sort_substring(self, seq):
+        """
+        Return a counter of all k-mers from a sequence
+        """
         res = [seq[i:i + self.seq_length] for i in range(len(seq) - self.seq_length + 1)]
         return Counter(res)
 
     def spectrum_ps(self, C1, C2):
+        """
+        Scalar product between two counters
+        """
         ps = 0
         for parameter, value in C1.items():
             if parameter in C2:
                 ps += value * C2[parameter]
         return ps
 
-    def embed(self, normalization=False):
-        count_list = []
-        for i in range(len(self.X_train)):
-            count_list.append(self.sort_substring(self.X_train[i]))
-        K = np.zeros((self.X_train.shape[0], self.X_train.shape[0]))
-        for i in range(0, self.X_train.shape[0]):
-            for j in range(0, i + 1):
-                ps = self.spectrum_ps(count_list[i], count_list[j])
+    def embed(self, X1, X2):
+        """
+        Return a matrix K(x_i, x_j) with x_i in X1 and x_j in X2
+        """
+        count_list1 = []
+        count_list2 = []
+        for i in range(len(X1)):
+            count_list1.append(self.sort_substring(X1[i]))
+        for i in range(len(X2)):
+            count_list2.append(self.sort_substring(X2[i]))
+
+        K = np.zeros((X1.shape[0], X2.shape[0]))
+        for i in range(0, X1.shape[0]):
+            for j in range(0, X2.shape[0]):
+                ps = self.spectrum_ps(count_list1[i], count_list2[j])
                 K[i, j] = ps
-                if i != j:
-                    K[j, i] = ps
-        K_norm = np.zeros((self.X_train.shape[0], self.X_train.shape[0]))
+
         # normalization
-        if normalization:
-            for i in range(0, self.X_train.shape[0]):
-                for j in range(i + 1):
-                    norm = K[i, j] / np.sqrt(K[i, i] * K[j, j])
-                    K_norm[i, j] = norm
-                    if i != j:
-                        K_norm[j, i] = norm
+        if self.normalization:
+            K_norm = np.zeros((X1.shape[0], X2.shape[0]))
+            for i in range(0, X1.shape[0]):
+                norm_i = self.spectrum_ps(count_list1[i], count_list1[i])
+                for j in range(X2.shape[0]):
+                    norm_j = self.spectrum_ps(count_list2[j], count_list2[j])
+                    K_norm[i, j] = K[i, j] / np.sqrt(norm_i * norm_j)
             return K_norm
         return K
 
+    def fit(self, X_train):
+        """
+        Return the train matrix
+        """
+        self.X_train = X_train
+        self.K = self.embed(X_train, X_train)
+        return self.K
+
+    def predict(self, X_test):
+        """
+        Return the test compute_occurence_matrix
+        """
+        self.K_test = self.embed(X_test, self.X_train)
+        return self.K_test
+
     def create_dict(self, seq_length):
+        """
+        Create a mapping vector
+        """
         if seq_length == 1:
             d = {'C': 0, 'A': 0, 'T': 0, 'G': 0}
         else:
@@ -63,6 +87,9 @@ class SpectrumKernel:
         return d
 
     def lookup_table(self, alpha, normalization=False):
+        """
+        Create a lookup table of all possible k-mers
+        """
         table = self.create_dict(self.seq_length)
         norm = 1.
         for i in range(self.X_train.shape[0]):
@@ -74,15 +101,3 @@ class SpectrumKernel:
                 for subseq, value in C.items():
                     table[subseq] += value * alpha[i].item() / norm
         self.table = table
-
-    def predict(self, X, true_value=False):
-        """
-        :param X: array, data to make predictions on.
-        :param true_value: bool, if True, return float values, if False, return binary array
-        """
-        label = np.zeros(X.shape[0])
-        for i in range(X.shape[0]):
-            for j in range(len(X[i]) - self.seq_length + 1):
-                label[i] += self.table[X[i][j:j + self.seq_length]]
-        return label
-
